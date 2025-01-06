2d80fe2148347f25f348c708b49e1ae2069c93c2
soban-mahmood <sobanmehmood4343@gmail.com>
2025-01-06 17:41:52 +0500

    .

--- Frontend/src/Pages/UserSignup.jsx
+++ Frontend/src/Pages/UserSignup.jsx
@@ -31,17 +31,17 @@ const UserSignup = () => {
       email: email,
       password: password
     }
-    
+
     const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser)
 
     if (response.status === 201) {
       const data = response.data
       setUser(data.user)
-      // localStorage.setItem('token', data.token)
+      localStorage.setItem('token', data.token)
       navigate('/home')
     }
 
-    console.log(user)
+
     setEmail('')
     setFirstName('')
     setLastName('')
