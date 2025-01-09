51ae42786f3b59df9860792f92f5e8c3349d2213
soban-mahmood <sobanmehmood4343@gmail.com>
2025-01-09 23:35:03 +0500

    complete the home page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,16 +1,41 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import logo from "../assets/Uber_logo_2018.png";
-
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import "remixicon/fonts/remixicon.css";
+import LocationSearchPanal from "../components/LocationSearchPanal";
 const Home = () => {
   const [pickUp, setPickUp] = useState("");
   const [dropOff, setDropOff] = useState("");
+  const [panalOpen, setPanalOpen] = useState(false);
+  const panalRef = useRef(null);
+  const panalCloseRef = useRef(null);
+
+  useGSAP(() => {
+    if (panalOpen) {
+      gsap.to(panalRef.current, {
+        hidden: false,
+        height: "70%",
+        padding:24
+      });
+      gsap.to(panalCloseRef.current, {
+        opacity: "1",
+      });
+    } else {
+      gsap.to(panalRef.current, {
+        hidden: true,
+        height: "0%",
+      });
+      gsap.to(panalCloseRef.current, {
+        opacity: "0",
+      });
+    }
+  }, [panalOpen]);
 
   const submitHandler = (e) => {
     e.preventDefault();
     setPickUp(pickUp);
     setDropOff(dropOff);
-    console.log(pickUp,dropOff)
-    console.log("Form submitted");
   };
   return (
     <div className="h-screen">
@@ -23,7 +48,16 @@ const Home = () => {
         />
       </div>
       <div className="absolute h-screen w-full top-0 flex justify-end flex-col">
-        <div className="h-[30%] p-5 bg-white relative">
+        <div className="h-[30%] p-5 bg-white relative  rounded-t-2xl">
+          <h5
+            onClick={() => {
+              setPanalOpen(!true);
+            }}
+            className=" absolute top-5  right-5"
+            ref={panalCloseRef}
+          >
+            <i className="ri-arrow-down-wide-line text-2xl"></i>
+          </h5>
           <h4 className="text-xl font-semibold mb-2">Find a trip</h4>
           <form className="" onSubmit={submitHandler}>
             {/* <div className="line h-16 w-[3px] bg-black absolute bottom-[25%] left-[10%] rounded-full lg:hidden md:hidden"></div> */}
@@ -33,6 +67,9 @@ const Home = () => {
               className="border p-2 w-full rounded-md mt-2 bg-[#EEEEEE] font-semibold px-8"
               value={pickUp}
               onChange={(e) => setPickUp(e.target.value)}
+              onClick={() => {
+                setPanalOpen(true);
+              }}
             />
             <input
               type="text"
@@ -40,11 +77,15 @@ const Home = () => {
               className="border p-2 w-full rounded-md mt-3 bg-[#EEEEEE] font-semibold px-8"
               value={dropOff}
               onChange={(e) => setDropOff(e.target.value)}
+              onClick={() => {
+                setPanalOpen(true);
+              }}
             />
-            <input type="submit" className="border p-2 w-1/2 rounded-md mt-3 bg-[#EEEEEE] font-semibold px-8" />
           </form>
         </div>
-        <div className="h-[70%] bg-red-800 hidden"></div>
+        <div className=" bg-white h-0 " ref={panalRef}>
+          <LocationSearchPanal />
+        </div>
       </div>
     </div>
   );
---
+++ Frontend/src/components/LocationSearchPanal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LocationSearchPanal = () => {
+  return (
+    <div>
+      LocationSearchPanal
+    </div>
+  )
+}
+
+export default LocationSearchPanal
