8fc194cf2592913dcda233e7d2e2e275078e3d37
soban-mahmood <sobanmehmood4343@gmail.com>
2025-01-12 10:52:19 +0500

    .

--- Frontend/src/components/LocationSearchPanal.jsx
+++ Frontend/src/components/LocationSearchPanal.jsx
@@ -5,25 +5,12 @@ const LocationSearchPanal = () => {
     <div div className='flex flex-col gap-8'>
     <div>
     <div className="flex items-center gap-4">
-      <h2 className='text-[20px] font-semibold'><i class="ri-map-pin-fill"></i></h2>
-      <h4 >24B , Near Kapoors Cafe Soban Coding School</h4>
+      <h2 className='bg-[#eeeeee] rounded-3xl h-10 flex items-center justify-center w-10 font-semibold'><i class="ri-map-pin-fill"></i></h2>
+      <h4>24B , Near Kapoors Cafe Soban Coding School</h4>
      </div>
     <hr />
     </div>
-    <div>
-    <div className="flex items-center gap-4">
-      <h2 className='text-[20px] font-semibold'><i class="ri-map-pin-fill"></i></h2>
-      <h4 >24B , Near Kapoors Cafe Soban Coding School</h4>
-     </div>
-    <hr />
-    </div>
-    <div>
-    <div className="flex items-center gap-4">
-      <h2 className='text-[20px] font-semibold'><i class="ri-map-pin-fill"></i></h2>
-      <h4 >24B , Near Kapoors Cafe Soban Coding School</h4>
-     </div>
-    <hr />
-    </div>
+   
     </div>
      
   )
