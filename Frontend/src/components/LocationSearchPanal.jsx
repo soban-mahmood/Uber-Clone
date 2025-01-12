bed7fa99a061eb54733d93029b62920914ee2bab
soban-mahmood <sobanmehmood4343@gmail.com>
2025-01-12 10:46:53 +0500

    ./

--- Frontend/src/components/LocationSearchPanal.jsx
+++ Frontend/src/components/LocationSearchPanal.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const LocationSearchPanal = () => {
   return (
+    <div div className='flex flex-col gap-8'>
     <div>
-      LocationSearchPanal
+    <div className="flex items-center gap-4">
+      <h2 className='text-[20px] font-semibold'><i class="ri-map-pin-fill"></i></h2>
+      <h4 >24B , Near Kapoors Cafe Soban Coding School</h4>
+     </div>
+    <hr />
     </div>
+    <div>
+    <div className="flex items-center gap-4">
+      <h2 className='text-[20px] font-semibold'><i class="ri-map-pin-fill"></i></h2>
+      <h4 >24B , Near Kapoors Cafe Soban Coding School</h4>
+     </div>
+    <hr />
+    </div>
+    <div>
+    <div className="flex items-center gap-4">
+      <h2 className='text-[20px] font-semibold'><i class="ri-map-pin-fill"></i></h2>
+      <h4 >24B , Near Kapoors Cafe Soban Coding School</h4>
+     </div>
+    <hr />
+    </div>
+    </div>
+     
   )
 }
 
